4e123035e4b2ef89377e19eb197fd0a0974ae53b
mahavasu <mahavasu@gmail.com>
2015-05-04 12:14:10 -0500

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,36 +1,30 @@
 -- Table definitions for the tournament project.
 
-
--- Dropping all the Table and views after making sure is that one exists 
+-- Dropping all the Table and views after making sure is that one exists
 DROP VIEW IF EXISTS standings CASCADE;
 DROP TABLE IF EXISTS matches CASCADE;
 DROP TABLE IF EXISTS players CASCADE;
 
-
-
-
 -- creating tables for storing details of the players
 
 CREATE TABLE players(
-	Player_id serial primary key,
-	Player_name text
-        );
-
+    Player_id serial primary key,
+    Player_name text
+    );
 
 --creating Table to store the details of the match
+-- included Result column to handle tie match
 CREATE TABLE matches(
-	Match_id serial primary key,
-	Player1_id integer references players,
-	Player2_id integer references players,
-	Result integer references players -- EXTRA COLUMN included to handle tie match for extra credit
-        );
+    Match_id serial primary key,
+    Player1_id integer references players,
+    Player2_id integer references players,
+    Result integer references players
+    );
 
---creating a view to store the details of the player and the matches they won and played
+-- creating a view to store the details of the player
+-- and the matches they won and played
 CREATE VIEW standings as
-	SELECT players.Player_id as Player_id, Player_name,
-        (SELECT COUNT(*) FROM matches WHERE matches.Result = players.player_id) as matches_won,
-        (SELECT COUNT(*) FROM matches WHERE players.player_id in (player1_id, player2_id) ) as matches_played
-	FROM players
-	GROUP BY players.player_id
-	ORDER BY matches_won DESC;
-
+    SELECT players.Player_id as Player_id, Player_name,
+    (SELECT COUNT(*) FROM matches WHERE matches.Result = players.player_id) as matches_won,
+    (SELECT COUNT(*) FROM matches WHERE players.player_id in (player1_id, player2_id) ) as matches_played 
+    FROM players GROUP BY players.player_id ORDER BY matches_won DESC;
